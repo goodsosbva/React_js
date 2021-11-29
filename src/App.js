04fcb51cdc6057a41ff8a34fe1921add41a10a8d
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-11-29 23:13:32 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
+import World from './component/World';
+import styles from "./App.module.css";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Hello/>
+      <div className={styles.box}>App</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// 퍼포먼스 관련내용
+reportWebVitals();
