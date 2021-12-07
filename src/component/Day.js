f4ae3aedaaca4a30b3a72aec2dad8f44e0b449cb
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-12-07 19:30:09 +0900

    Add files via upload

---
+++ src/component/Day.js
@@ -0,0 +1,23 @@
+import dummy from "../db/data.json";
+import { useParams } from "react-router";
+import Word from "./Word";
+
+export default function Day() {
+    const a = useParams()
+    const day = a.day;
+    const wordList = dummy.words.filter(word => word.day === Number(day));
+
+    
+    return (
+    <>
+        <h2>Day {day}</h2>
+        <table>
+            <tbody>
+                {wordList.map(word => (
+                 <Word word={word} key={word.id} />
+                ))}
+            </tbody>
+        </table>
+    </>
+    );
+}
