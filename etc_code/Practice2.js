98dd201b3f62337eb88aa231028ae1e0563d50e8
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-12-15 19:11:19 +0900

    Create Practice2.js

---
+++ etc_code/Practice2.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* 이런식으로 작성해야 주석처리가 된다. */}
+        <h1>리액트</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
