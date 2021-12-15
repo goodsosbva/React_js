bb3d73c2894de9de6c9744f0e88164a2339feb0e
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-12-15 19:07:04 +0900

    Create Practice1.js

---
+++ etc_code/Practice1.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">안냥! 아년냥안</div>;
+  }
+}
+
+export default App;
