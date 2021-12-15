677e9afc19cd552bed40d1a5501ad174005e49a7
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-12-15 20:04:44 +0900

    Create Practice3.js

---
+++ etc_code/Practice3.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import MyName from "./MyName";
+
+class App extends Component {
+  render() {
+    return <MyName name="왜안되냐?" />;
+  }
+}
+
+export default App;
