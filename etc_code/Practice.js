1ab1a2f384af463fab45ea583f641f9f5ffe6e27
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-12-15 18:58:34 +0900

    Create Practice.js

---
+++ etc_code/Practice.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const style = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '36px'
+    };
+    return (
+      <div style={style}>
+        안냥!
+      </div>
+    );
+  }
+}
+
+export default App;
