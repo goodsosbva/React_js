5aeb2a468652b260e26dfd962093f642c1373c94
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-12-16 22:13:36 +0900

    Create Practice4.js

---
+++ etc_code/Practice4.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Counter from "./Counter.js";
+
+class App extends Component {
+  render() {
+    return <Counter />;
+  }
+}
+
+export default App;
